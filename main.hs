99ec48ba0b91a7f728c2d4053a9e003bdd1849ca
ruslashev <ruslashev@gmail.com>
2014-01-18 22:48:12 +0400

    Find for files from includes, first proper version

--- main.hs
+++ main.hs
@@ -4,8 +4,8 @@ import Control.Monad (forM_)
 
 source = unlines [
     "digraph G {",
-    "\tnode [shape=box/*,fixedsize=true,width=2.0*/];",
-    "\toverlap=false"
+    "\tgraph [splines=true,overlap=scale]",
+    "\tnode [shape=box,fontname=\"Sans\",fontsize=12.0];"
     ]
 
 main = do
@@ -20,28 +20,63 @@ main = do
     startScan $ "link-to-dir" -- head args
 
 startScan :: FilePath -> IO ()
-startScan dir = do
+startScan directory = do
+    let dir = reverse $ dropWhile (== '/') $ reverse directory
+
     files <- getFileList dir
-    let srcFiles = filter (\ x -> dropWhile (/= '.') x `elem`
+    let srcFilesList = filter (\ x -> dropWhile (/= '.') x `elem`
                      [".cpp", ".c", ".cxx", ".cc", ".h", ".hh", ".hpp", ".hxx"])
                      files
-    strippedFiles <- mapM getPPdirectives srcFiles
-    let filesAndTheirIncludes = zip srcFiles $ map scanFile strippedFiles
+
     putStrLn source
-    forM_ filesAndTheirIncludes (\ (file,incs) -> do
-            forM_ incs (\ inc ->
-                let unrelFile = drop (length dir + 1) file
-                in putStrLn $ "\t" ++ show unrelFile ++ " -> " ++ show inc
+
+    forM_ srcFilesList (\ file -> do
+            stripped <- getPPdirectives file
+            let includes = scanForIncludes stripped
+            relIncludes <- getRelIncludes file includes
+            forM_ relIncludes (\ inc -> do
+                putStrLn $ "\t" ++ show file ++ " -> " ++ show inc
                 )
-        )
+            )
+
     putStrLn "}"
 
-scanFile :: [String] -> [String]
-scanFile [] = []
-scanFile (x:xs) =
+getRelIncludes :: String -> [String] -> IO [String]
+getRelIncludes _ [] = return []
+getRelIncludes file incs@(x:xs) = do
+    path <- scanInclude file x
+    rest <- getRelIncludes file xs
+    return $ path : rest
+
+scanInclude :: String -> String -> IO String
+scanInclude file inc@(x:xs) = do
+    if x == '<' then do
+        return inc
+    else
+        let brokenPath = break (== '/') $ reverse file
+            path = reverse $ drop 1 $ snd brokenPath
+            fileName = reverse $ fst brokenPath
+        in
+        if take 2 xs == "./" then do
+            inc <- scanInclude file ("\"" ++ drop 3 xs)
+            return inc
+        else
+            if take 3 xs == "../" then
+                let backDir = reverse $ drop 1 $ dropWhile (/= '/') $ reverse path
+                in do
+                    scanInclude (backDir ++ "/" ++ fileName) ("\"" ++ drop 3 xs)
+            else
+                if x == '"' then do
+                    return $ path ++ "/" ++ init xs
+                else do -- #defined path
+                    return inc
+
+scanForIncludes :: [String] -> [String]
+scanForIncludes [] = []
+scanForIncludes (x:xs) =
     let (kind,arg) = break (== ' ') x in
-        case kind of "#include" -> tail arg : scanFile xs
-                     _          -> scanFile xs
+        case kind of "#include" -> tail arg : scanForIncludes xs
+                     _          -> scanForIncludes xs
 
 getPPdirectives :: FilePath -> IO ([String])
 getPPdirectives file = do
