5c9f63bb8e7f8b66df91d8d8133c35c09adfd5b4
ruslashev <ruslashev@gmail.com>
2014-08-24 18:41:56 +0400

    Style corrected code thanks to hlint

--- cdepgraph.hs
+++ cdepgraph.hs
@@ -1,6 +1,7 @@
-import Control.Monad (forM_)
+import Control.Monad (forM_,when)
 import Data.Char (toLower)
 import Data.List (foldl')
+import Data.Maybe (isNothing)
 import qualified Data.Text as T
 import qualified Data.Text.IO as Tio
 import System.Environment (getArgs, getProgName)
@@ -13,7 +14,7 @@ main = do
     if length args /= 1 then do
         progName <- getProgName
         putStrLn $ "Usage: " ++ progName ++ " <directory>"
-        putStrLn $ "Start a scan for source files in specified directory"
+        putStrLn   "Start a scan for source files in specified directory"
     else
         startScan $ head args
 
@@ -26,10 +27,9 @@ header = unlines [
 startScan :: String -> IO ()
 startScan dir = do
     filesMaybe <- getFileList dir
-    if filesMaybe == Nothing then do
+    when (isNothing filesMaybe) $ do
         putStrLn $ "Failed to open directory \"" ++ dir ++ "\""
         exitFailure
-    else return ()
     let files = case filesMaybe of Just list -> list
 
     let srcFilesList = filter isSrcFile files
@@ -52,21 +52,21 @@ startScan dir = do
               `elem` ["cpp", "c", "cxx", "cc", "cp", "c++",
                       "hpp", "h", "hxx", "hh", "hp", "h++"]
 
-getIncludes :: FilePath -> IO ([T.Text])
+getIncludes :: FilePath -> IO [T.Text]
 getIncludes file = do
     fileContents <- Tio.readFile file
     let ppDirectives = (filter (\ line -> T.head line == '#') .
-                        filter (\ line -> not $ T.null line) .
+                        filter (not . T.null) .
                         T.lines) fileContents
     let includedFiles = foldl' (\ acc d ->
             let (kind,arg) = T.break (== ' ') d
-            in if kind == (T.pack "#include")
-               then (T.tail arg) : acc
+            in if kind == T.pack "#include"
+               then T.tail arg : acc
                else acc) [] ppDirectives
     return includedFiles
 
 getRelIncludes :: FilePath -> [T.Text] -> [T.Text]
-getRelIncludes file includes = map (makeRelative file) includes
+getRelIncludes file = map (makeRelative file)
 
 makeRelative :: FilePath -> T.Text -> T.Text
 makeRelative file include =
@@ -78,23 +78,23 @@ makeRelative file include =
             let upperDirectory =
                     reverse $ drop 1 $ dropWhile (/= '/') $ reverse fileDir
             in makeRelative (upperDirectory ++ "/")
-                            ((T.pack "\"") `T.append` (T.drop 3 xs))
+                            (T.pack "\"" `T.append` T.drop 3 xs)
         else
-            if T.take 2 xs == (T.pack "./") then
-                makeRelative file ((T.pack "\"") `T.append` (T.drop 3 xs))
+            if T.take 2 xs == T.pack "./" then
+                makeRelative file (T.pack "\"" `T.append` T.drop 3 xs)
             else
-                (T.pack fileDir) `T.append` (T.cons '/' (T.init xs))
+                T.pack fileDir `T.append` T.cons '/' (T.init xs)
     else -- angular brackets or #defined path
         include
     where x  = T.head include
           xs = T.tail include
 
 colorizeNode :: String -> String
-colorizeNode inc =
-    if head inc == '<' then blue
-    else if fileExt `elem` ["cpp", "c", "cxx", "cc", "cp", "c++"] then yellow
-    else if fileExt `elem` ["hpp", "h", "hxx", "hh", "hp", "h++"] then green
-    else red
+colorizeNode inc
+    | head inc == '<' = blue
+    | fileExt `elem` ["cpp", "c", "cxx", "cc", "cp", "c++"] = yellow
+    | fileExt `elem` ["hpp", "h", "hxx", "hh", "hp", "h++"] = green
+    | otherwise = red
     where fileExt = reverse $ takeWhile (/= '.') $ reverse inc
           blue   = "color=\"#D5EEFB\""
           yellow = "color=\"#F8F8D3\""
