bcd2b8d5e3a04253f679c329a5e07c692fc52baf
ruslashev <ruslashev@gmail.com>
2014-08-30 21:59:51 +0400

    Refactoring more

--- cdepgraph.hs
+++ cdepgraph.hs
@@ -23,6 +23,9 @@ header = unlines [
     "{",
     '\t' : "graph [splines=true, overlap=scale]",
     '\t' : "node [shape=box, style=filled, fontname=\"Sans\", fontsize=12.0];" ]
+sourceFileExtensions = ["cpp", "c", "cxx", "cc", "cp", "c++"]
+headerFileExtensions = ["hpp", "h", "hxx", "hh", "hp", "h++"]
+includeText = T.pack "#include"
 
 startScan :: String -> IO ()
 startScan dir = do
@@ -44,16 +47,14 @@ startScan dir = do
 
 isSourceFile :: String -> Bool
 isSourceFile file =
-    extension `elem`
-         ["cpp", "c", "cxx", "cc", "cp", "c++",
-          "hpp", "h", "hxx", "hh", "hp", "h++"]
+    extension `elem` sourceFileExtensions ++ headerFileExtensions
     where extension = (map toLower . reverse . takeWhile (/= '.') . reverse) file
 
 mainOutput :: [String] -> IO ()
 mainOutput [] = putStrLn "}"
 mainOutput (file:files) = do
     includes <- getIncludes file
-    let relIncludes = getRelIncludes file includes
+    let relIncludes = map (makeRelative file) includes
     forM_ relIncludes (\ inc -> do
         putStrLn $ "\tnode [" ++ colorizeNode (T.unpack inc) ++ "]"
         putStrLn $ "\t" ++ show file ++ " -> " ++ show inc
@@ -65,25 +66,18 @@ mainOutput (file:files) = do
 getIncludes :: String -> IO [T.Text]
 getIncludes file = do
     fileContents <- Tio.readFile file
-    let ppDirectives = (filter (\ line -> T.head line == '#') .
-                        filter (not . T.null) .
-                        T.lines) fileContents
-    let includedFiles = foldl' (\ acc d ->
-            let (kind,arg) = T.break (== ' ') d
-            in if kind == T.pack "#include"
-               then T.tail arg : acc
-               else acc) [] ppDirectives
+    let includedFiles =
+            [ T.tail $ T.dropWhile (/= ' ') line
+            | line <- T.lines fileContents
+            , not $ T.null line
+            , T.take 8 line == includeText
+            ]
     return includedFiles
 
-getRelIncludes :: FilePath -> [T.Text] -> [T.Text]
-getRelIncludes file = map (makeRelative file)
-
-makeRelative :: FilePath -> T.Text -> T.Text
-makeRelative file include =
+makeRelative :: String -> T.Text -> T.Text
+makeRelative filename include =
     if x == '"' then
-        let brokenPath = break (== '/') $ reverse file
-            fileDir = reverse $ tail $ snd brokenPath
-            fileName = reverse $ fst brokenPath in
+        let fileDir = reverse $ tail $ dropWhile (/= '/') $ reverse filename in
         if T.take 3 xs == T.pack "../" then
             let upperDirectory =
                     reverse $ drop 1 $ dropWhile (/= '/') $ reverse fileDir
@@ -91,7 +85,7 @@ makeRelative file include =
                             (T.pack "\"" `T.append` T.drop 3 xs)
         else
             if T.take 2 xs == T.pack "./" then
-                makeRelative file (T.pack "\"" `T.append` T.drop 3 xs)
+                makeRelative filename (T.pack "\"" `T.append` T.drop 3 xs)
             else
                 T.pack fileDir `T.append` T.cons '/' (T.init xs)
     else -- angular brackets or #defined path
@@ -102,8 +96,8 @@ makeRelative file include =
 colorizeNode :: String -> String
 colorizeNode inc
     | head inc == '<' = blue
-    | fileExt `elem` ["cpp", "c", "cxx", "cc", "cp", "c++"] = yellow
-    | fileExt `elem` ["hpp", "h", "hxx", "hh", "hp", "h++"] = green
+    | fileExt `elem` sourceFileExtensions = yellow
+    | fileExt `elem` headerFileExtensions = green
     | otherwise = red
     where fileExt = reverse $ takeWhile (/= '.') $ reverse inc
           blue   = "color=\"#D5EEFB\""
