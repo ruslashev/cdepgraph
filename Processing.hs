bf5a78db75c90545b13cd8c36d64c8f53305b62e
ruslashev <ruslashev@gmail.com>
2015-01-20 22:17:39 +0300

    Work a bit on Processing.hs, no major changes

--- Processing.hs
+++ Processing.hs
@@ -8,7 +8,13 @@ import qualified Data.Text.IO as Tio
 
 data SrcFile = SrcFile { name :: T.Text
                        , includes :: [T.Text]
-                       }
+                       } deriving (Show)
+
+getDirectory :: T.Text -> T.Text
+getDirectory = T.reverse . T.drop 1 . T.dropWhile (/= '/') . T.reverse
+
+getExtension :: String -> String
+getExtension = map toLower . reverse . takeWhile (/= '.') . reverse
 
 sourceFileExts, headerFileExts :: [String]
 sourceFileExts = ["cpp", "c", "cxx", "cc", "cp", "c++"]
@@ -16,8 +22,7 @@ headerFileExts = ["hpp", "h", "hxx", "hh", "hp", "h++"]
 
 isSourceFile :: String -> Bool
 isSourceFile file =
-    extension `elem` (sourceFileExts ++ headerFileExts)
-    where extension = (map toLower . reverse . takeWhile (/= '.') . reverse) file
+    getExtension file `elem` (sourceFileExts ++ headerFileExts)
 
 process :: [String] -> IO [SrcFile]
 process = mapM getSrcFile . filter isSourceFile
@@ -29,7 +34,7 @@ getSrcFile file = do
     return $ SrcFile fileAsText absIncludes
 
 getIncludes :: T.Text -> [T.Text]
-getIncludes = map (T.tail . T.dropWhile (/= ' '))
+getIncludes = map (T.drop 1 . T.dropWhile (/= ' '))
             . filter ((includeText ==) . T.take (T.length includeText))
             . T.lines
     where includeText = T.pack "#include"
@@ -37,10 +42,9 @@ getIncludes = map (T.tail . T.dropWhile (/= ' '))
 convToAbs :: T.Text -> T.Text -> T.Text
 convToAbs file include =
     if x == '"' then
-        let fileDir = T.reverse $ T.tail $ T.dropWhile (/= '/') $ T.reverse file in
+        let fileDir = getDirectory file in
         if T.take 3 xs == T.pack "../" then
-            let upperDirectory =
-                    T.reverse $ T.drop 1 $ T.dropWhile (/= '/') $ T.reverse fileDir
+            let upperDirectory = getDirectory fileDir
             in convToAbs (upperDirectory `T.append` T.pack "/")
                             (T.pack "\"" `T.append` T.drop 3 xs)
         else
