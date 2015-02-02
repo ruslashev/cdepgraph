8ab6f1c12aac593c2ae4463074173a91cad61527
ruslashev <ruslashev@gmail.com>
2015-02-02 20:23:47 +0300

    Finish Processing.Clusters

--- src/Processing/Clusters.hs
+++ src/Processing/Clusters.hs
@@ -1,23 +1,31 @@
 module Processing.Clusters
 where
 
-import Data.List (nub, elemIndex)
+import Data.List (elemIndex, nub, partition, sort)
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import System.FilePath.Posix (splitFileName,splitDirectories)
 
 import Processing.Includes
 
-data Directory = Folder String Directory | File String deriving (Show)
+data Cluster = SystemInclude T.Text
+             | Folder T.Text Cluster
+             | File T.Text
+             deriving (Show)
 data SrcFileI = SrcFileI { nameI :: Int
                          , includesI :: [Int]
                          } deriving (Show)
 
--- clusterize :: [SrcFile] -> [Directory]
--- clusterize srcFiles =
---     let lookupList = nub $ sort $ srcFilesToList srcFiles
---         srcFilesI = makeSrcFilesI lookupList srcFiles
---     in
+clusterize :: [SrcFile] -> ([Cluster], [SrcFileI])
+clusterize srcFiles =
+    let filesAndIncludes = nub $ sort $ srcFilesToList srcFiles
+        (fileIncludes,systemIncludes) =
+            partition (\ x -> T.head x == '/') filesAndIncludes
+        lookupList = systemIncludes ++ fileIncludes
+        srcFilesI = makeSrcFilesI lookupList srcFiles
+        fileIncsRepeatsRemoved = removeRepeats fileIncludes
+        clusters = makeClusters fileIncsRepeatsRemoved systemIncludes
+    in (clusters, srcFilesI)
 
 srcFilesToList :: [SrcFile] -> [T.Text]
 srcFilesToList [] = []
@@ -31,11 +39,20 @@ makeSrcFilesI lookupList (SrcFile name includes : rest) =
         where lookup :: T.Text -> Int
               lookup key = fromMaybe 0 (elemIndex key lookupList)
 
-breakPath :: String -> [String]
-breakPath = filter (/= "/") . splitDirectories
+removeRepeats :: [T.Text] -> [T.Text]
+removeRepeats list@(x:_) =
+    if all (\ y -> T.head x == T.head y) list
+    then removeRepeats (map T.tail list)
+    else list
 
-textToDirectory :: String -> Directory
-textToDirectory str = aux $ breakPath str --(T.unpack str)
-    where aux (x:[]) = File x
-          aux (x:xs) = Folder x (aux xs)
+makeClusters :: [T.Text] -> [T.Text] -> [Cluster]
+makeClusters fileIncludes sysIncludes =
+    map fileToCluster fileIncludes ++
+    map SystemInclude sysIncludes
+
+fileToCluster :: T.Text -> Cluster
+fileToCluster str = aux $ breakPath str
+    where aux (x:[]) = File (T.pack x)
+          aux (x:xs) = Folder (T.pack x) (aux xs)
+          breakPath = filter (/= "/") . splitDirectories . T.unpack
 
