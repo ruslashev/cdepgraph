64ebbce06cbb9c4dfc23161a93838b22ecf32067
ruslashev <ruslashev@gmail.com>
2015-04-28 15:40:19 +0300

    Making progress on proper clusters

--- src/Processing/Clusters.hs
+++ src/Processing/Clusters.hs
@@ -8,6 +8,7 @@ import Data.List (elemIndex, nub, partition, sort)
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import System.FilePath.Posix (splitFileName,splitDirectories)
+import Debug.Trace
 
 import Processing.Includes
 
@@ -16,7 +17,7 @@ import Processing.Includes
 -- the ones that are used as (#include "header.hpp"). Since all includes
 -- are turned to clusters, other headers are defined as "SystemInclude"
 data Cluster = SystemInclude T.Text
-             | Folder [Cluster]
+             | Folder T.Text [Cluster]
              | File T.Text
              deriving (Show)
 
@@ -39,15 +40,6 @@ srcFilesToList [] = []
 srcFilesToList (SrcFile name includes : rest) =
     name : includes ++ srcFilesToList rest
 
-makeSrcFilesI :: [T.Text] -> [SrcFile] -> [SrcFileI]
-makeSrcFilesI _ [] = []
-makeSrcFilesI lookupList (SrcFile name includes : rest) =
-    SrcFileI (lookupI lookupList name) (map (lookupI lookupList) includes) :
-        makeSrcFilesI lookupList rest
-
-lookupI :: [T.Text] -> T.Text -> Int
-lookupI hay needle = fromMaybe 0 (elemIndex needle hay)
-
 removeLeadingRepeats :: [T.Text] -> [T.Text]
 removeLeadingRepeats list@(x:_) =
     if all (\ y -> T.head x == T.head y) list
@@ -56,12 +48,24 @@ removeLeadingRepeats list@(x:_) =
 
 makeClusters :: [T.Text] -> [T.Text] -> [Cluster]
 makeClusters fileIncludes sysIncludes =
-    map fileToCluster fileIncludes ++
-    map SystemInclude sysIncludes
+    map SystemInclude sysIncludes ++
+    filesToClusters fileIncludes
 
-fileToCluster :: T.Text -> Cluster
-fileToCluster str = aux $ breakPath str
-    where aux (x:[]) = File (T.pack x)
-          aux (x:xs) = Folder (T.pack x) (aux xs)
-          breakPath = filter (/= "/") . splitDirectories . T.unpack
+filesToClusters :: [T.Text] -> [Cluster]
+filesToClusters files =
+    map (strToCluster . breakPath) files
+    where strToCluster :: [T.Text] -> Cluster
+          strToCluster (x:[]) = File x
+          strToCluster (x:xs) = Folder x [strToCluster xs]
+          breakPath :: T.Text -> [T.Text]
+          breakPath = map T.pack . filter (/= "/") . splitDirectories . T.unpack
+
+makeSrcFilesI :: [T.Text] -> [SrcFile] -> [SrcFileI]
+makeSrcFilesI _ [] = []
+makeSrcFilesI lookupList (SrcFile name includes : rest) =
+    SrcFileI (lookupI lookupList name) (map (lookupI lookupList) includes) :
+        makeSrcFilesI lookupList rest
+
+lookupI :: [T.Text] -> T.Text -> Int
+lookupI hay needle = fromMaybe 0 (elemIndex needle hay)
 
